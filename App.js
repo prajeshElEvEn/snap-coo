00990af10bde25dfa32fa6796aebc5353aa93acd
Vedasto <prajesh.eleven118@gmail.com>
2022-12-06 23:38:43 +0530

    initialized snapcoo - installed bare min

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SettingScreen from './screens/SettingScreen';
+import CaptureScreen from './screens/CaptureScreen';
+import EditScreen from './screens/EditScreen';
+import GalleryScreen from './screens/GalleryScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Capture" component={CaptureScreen} />
+        <Stack.Screen name="Edit" component={EditScreen} />
+        <Stack.Screen name="Gallery" component={GalleryScreen} />
+        <Stack.Screen name="Setting" component={SettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/CaptureScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CaptureScreen = () => {
+    return (
+        <View>
+            <Text>CaptureScreen</Text>
+        </View>
+    )
+}
+
+export default CaptureScreen
